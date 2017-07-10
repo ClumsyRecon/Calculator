585ac77cf3d0ffbd17dbe07a149d8a03a542647c
Daniel Johansson <theclumsyrecon@yahoo.com.au>
2017-07-10 14:07:33 +1000

    Fixed stuff
    It's better

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
-    <script type="text/javascript">
-
-    </script>
+    <title>Calculator</title>
   </head>
   <body>
     <form action="index.php" method="post">
-      <input type="number" name="num1" value="0">
+      <input type="number" name="num1" value=<?php if($_POST != null) { echo $_POST['num1'];} else { echo '0';} ?>>
       <select name="operator">
-        <option value="plus">+</option>
-        <option value="minus">-</option>
-        <option value="divide">/</option>
-        <option value="multiply">*</option>
+        <option value="plus" <?php if($_POST != null) { if($_POST['operator'] == 'plus') { echo 'selected'; } } ?>>+</option>
+        <option value="minus" <?php if($_POST != null) { if($_POST['operator'] == 'minus') { echo 'selected'; } } ?>>-</option>
+        <option value="divide" <?php if($_POST != null) { if($_POST['operator'] == 'divide') { echo 'selected'; } } ?>>/</option>
+        <option value="multiply" <?php if($_POST != null) { if($_POST['operator'] == 'multiply') { echo 'selected'; } } ?>>*</option>
       </select>
-      <input type="number" name="num2" value="0">
+      <input type="number" name="num2" value=<?php if($_POST != null) { echo $_POST['num2'];} else { echo '0';} ?>>
       <input type="submit" value="=">
       <span>
         <?php
